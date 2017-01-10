6c1870b037f247a31cbd2844341e675dfe009a05
Artyom Shandrakov <artyom.shandrakov@gamil.com>
2017-01-10 23:34:59 +0300

    Add gitignore.

--- app/src/test/java/com/shandrakov/sandbox/UserPresentersTest.java
+++ app/src/test/java/com/shandrakov/sandbox/UserPresentersTest.java
@@ -24,7 +24,6 @@ import java.util.List;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class UserPresentersTest {
